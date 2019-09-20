676ec5b593370fe267c54dd3d01b3cb83e10d8da
Vitaliy <55364824+OpLuK@users.noreply.github.com>
2019-09-20 11:39:12 +0300

    Add files via upload

--- kalendar/src/kalendar.java
+++ kalendar/src/kalendar.java
@@ -1,45 +1,46 @@
 import java.util.Scanner;
 public class kalendar {
 	public static void main (String[]args) {
-		System.out.print("Введите номер (1-12) года по старояпонскому календарю: ");
+		System.out.print("Введите номер года: ");
 		Scanner in = new Scanner (System.in);
-		int year = in.nextInt();
+		int year_n = in.nextInt();
 		in.close();
-		switch (year) {
-		case 1:
+		int year_k = year_n % 12;
+		switch (year_k) {
+		case 4:
 			System.out.println("Это год крысы.");
 			break;
-		case 2:
+		case 5:
 			System.out.println("Это год коровы.");
 			break;
-		case 3:
+		case 6:
 			System.out.println("Это год тигра.");
 			break;
-		case 4:
+		case 7:
 			System.out.println("Это год зайца.");
 			break;
-		case 5:
+		case 8:
 			System.out.println("Это год дракона.");
 			break;
-		case 6:
+		case 9:
 			System.out.println("Это год змеи.");
 			break;
-		case 7:
+		case 10:
 			System.out.println("Это год лошади.");
 			break;
-		case 8:
+		case 11:
 			System.out.println("Это год овцы.");
 			break;
-		case 9:
+		case 0:
 			System.out.println("Это год обезьяны.");
 			break;
-		case 10:
+		case 1:
 			System.out.println("Это год курицы.");
 			break;
-		case 11:
+		case 2:
 			System.out.println("Это год собаки.");
 			break;
-		case 12:
+		case 3:
 			System.out.println("Это год свиньи.");
 			break;
 		default :
